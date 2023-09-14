213f3e8e205bb550d07de256e70ba753a3995667
amanullahjn99 <amanullahjn99@gmail.com>
2023-09-13 22:25:07 -0400

    09/13

---
+++ src/class22/BikeTester.java
@@ -0,0 +1,12 @@
+package class22;
+
+public class BikeTester {
+    public static void main(String[] args) {
+        Vehicle h =new HondaBike();
+
+       /* Object c= new String();
+        Object s=new Scanner(System.in);
+        Object d=new Dog();
+        Object g=new HondaBike();*/
+    }
+}
---
+++ src/class22/MoveAbleTester.java
@@ -0,0 +1,13 @@
+package class22;
+
+public class MoveAbleTester {
+    public static void main(String[] args) {
+        MoveAble [] arr={new Car(),new Dog()};
+        for (MoveAble a:arr){
+            a.move();
+
+        }
+
+        // MoveAble m=new MoveAble();
+    }
+}
