eceb0cabf0e179a757aba0c08a7a5354fffc4cbe
amanullahjn99 <amanullahjn99@gmail.com>
2023-10-02 22:17:40 -0400

    02Oct

--- src/class28/HOMEWork.java
+++ src/class28/HOMEWork.java
@@ -1,4 +1,9 @@
 package class28;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /*@here Create a property file to store following configurations:
 browser=chrome
 url=https://facebook.com
@@ -7,7 +12,17 @@ password
 Read file and extract values of browser & url
    */
 public class HOMEWork {
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Users\\ahmad\\IdeaProjects\\Main\\Files\\PropertyFIle_HW.properties";
+        FileInputStream fis = new FileInputStream(path);
+        Properties load1 = new Properties();
+        load1.load(fis);
+        System.out.println(load1.getProperty("Browser"));
+        System.out.println(load1.getProperty("URL"));
+        System.out.println(load1.getProperty("username"));
+        System.out.println(load1.getProperty("password"));
 
 
+    }
 
 }
---
+++ src/review11/E1WrapperClasses.java
@@ -0,0 +1,26 @@
+package review11;
+
+public class E1WrapperClasses {
+    public static void main(String[] args) {
+
+        int a=10;
+        double b=a;
+        System.out.println(b);
+
+        int f=30;
+
+        Integer g=f;
+        System.out.println(g);
+
+        double u=30.5;
+        Double t=u;
+        System.out.println(u);
+
+        String name="dnfjdnf";
+
+        Boolean o=false;
+        boolean s=o;
+
+
+    }
+}
