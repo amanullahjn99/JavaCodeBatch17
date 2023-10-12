853b521c4b532286fc6e5e26874ae01fff95c1b4
amanullahjn99 <amanullahjn99@gmail.com>
2023-10-12 13:08:57 -0400

    09Oct

--- src/Review6/test.java
+++ src/Review6/test.java
@@ -1,4 +1,14 @@
 package Review6;
 
 public class test {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int result=5+5;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(result);
+
+        }
+    }
 }
